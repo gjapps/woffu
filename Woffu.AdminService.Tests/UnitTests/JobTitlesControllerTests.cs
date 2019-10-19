6484ae9b05df61209be5c6c46ff8cb32d991fb10
Gustavo <gustavoja@gmail.com>
2019-10-19 02:16:59 +0200

    Add tests scaffolding

---
+++ Woffu.AdminService.Tests/UnitTests/JobTitlesControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using Xunit;
+
+namespace Woffu.AdminService.Tests.UnitTests
+{
+    public class JobTitlesControllerTests
+    {
+        [Fact()]
+        public void UserLogged_GetJobTitles_JobTitlesReceived()
+        {
+            Assert.True(false);
+        }
+
+        [Fact()]
+        public void UserLogged_GetJobTitle_JobTitleReceived()
+        {
+            Assert.True(false);
+        }
+
+        [Fact()]
+        public void UserLogged_CreateUnexistantJobTitle_JobTitleCreated()
+        {
+            Assert.True(false);
+        }
+
+
+        [Fact()]
+        public void UserLogged_CreateExistantJobTitle_JobTitleCreationFail()
+        {
+            Assert.True(false);
+        }
+
+
+        [Fact()]
+        public void UserLogged_UpdateExistantJobTitle_JobTitleUpdated()
+        {
+            Assert.True(false);
+        }
+
+        [Fact()]
+        public void UserLogged_UpdateUnexistantJobTitle_JobTitleUpdateFail()
+        {
+            Assert.True(false);
+        }
+
+        [Fact()]
+        public void UserLogged_DeleteExistantJobTitle_JobTitleDeleted()
+        {
+            Assert.True(false);
+        }
+
+        [Fact()]
+        public void UserLogged_DeleteUnexistantJobTitle_JobTitleDeleteFail()
+        {
+            Assert.True(false);
+        }
+    }
+}
